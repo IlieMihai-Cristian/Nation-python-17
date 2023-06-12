392cf393ec928f3b209c806d3689c222b083baa0
mihai <mihai.ilie@nxdata.ro>
2023-06-12 21:44:53 +0300

    variabile

---
+++ curs01_Hello_World/variabile.py
@@ -0,0 +1,36 @@
+# a = 10
+# c = 10
+# b = 'pen'
+# x = None
+# print(a)
+
+a = 10  # integer
+b = 11.2  # float
+c = 'pen'  # string
+d = True
+e = False  # boolean
+f = 5j
+g = [10, 11.2, True, None]  # lista
+h = None  # nonetype
+i = (10, 12.3, 4j, ['sadjadh', 3, True], )  # tuple
+j = {1, 2, 3}  # set
+k = {1: 'z'}  # dict
+
+# print(a * b)
+# print(type(a * b))
+# a = '10'
+# # print(a * c)
+# print(a + c)
+
+a = a + 1
+a += 1
+
+# a venitul unui salarit
+venit_salariat = 10
+venitSalariat = 10
+venitsalariatpeora = 10
+
+# venit-salariat = 12
+# venit salariat = 12
+# 1venit_salariat = 12
+# (len, max, min, type, int, float, if, else, for, while, break)
